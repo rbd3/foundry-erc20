98c01eb115e9fbbb1144e7b0ad9486288f2936fc
rbd3 <rabedesanaandry01@gmail.com>
2025-04-01 16:41:54 +0300

    Update token

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,7 +2,31 @@
 pragma solidity ^0.8.28;
 
 contract ManualToken {
-    function name() public view returns (string memory) {
+    mapping(address => uint256) private s_balance;
+
+    function name() public pure returns (string memory) {
         return "Manual Token";
+    } //same as string public name = "Manual Token"
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return s_balance[_owner];
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
+        balanceOf(msg.sender) -= _value;
+        balanceOf(_to) += _value;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
     }
 }
