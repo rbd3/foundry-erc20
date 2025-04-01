b2ad29bfc1fcc9ec52b9d20f61352daf494877ea
rbd3 <rabedesanaandry01@gmail.com>
2025-04-01 16:25:58 +0300

    First commit

---
+++ src/ManualToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract ManualToken {
+    function name() public view returns (string memory) {
+        return "Manual Token";
+    }
+}
